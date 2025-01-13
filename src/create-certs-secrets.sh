a80d4727712aaaf67e1e7fdc29bb63359378968a
VenuG <gangineni.vg.bp@gmail.com>
2025-01-13 09:12:47 -0500

    kubernetes yml files

--- src/create-certs-secrets.sh
+++ src/create-certs-secrets.sh
@@ -1,6 +1,6 @@
-mkdir -p /ASP.NET/Https
-mkdir -p /Microsoft/UserSecrets
-dotnet dev-certs https -ep /ASP.NET/Https/localhost.pfx -p developer
+mkdir -p ${APPDATA}/ASP.NET/Https
+mkdir -p ${APPDATA}/Microsoft/UserSecrets
+dotnet dev-certs https -ep ${APPDATA}/ASP.NET/Https/localhost.pfx -p developer
 dotnet user-secrets -p WebApps/Shopping.Web/Shopping.Web.csproj set "Kestrel:Certificates:Default:Path" "/home/app/.aspnet/https/localhost.pfx" 
 dotnet user-secrets -p WebApps/Shopping.Web/Shopping.Web.csproj set "Kestrel:Certificates:Default:Password" "developer" 
 dotnet user-secrets -p Services/Basket/Basket.API/Basket.API.csproj set "Kestrel:Certificates:Default:Path" "/home/app/.aspnet/https/localhost.pfx"  
@@ -13,6 +13,6 @@ dotnet user-secrets -p Services/Ordering/Ordering.API/Ordering.API.csproj set "K
 dotnet user-secrets -p Services/Ordering/Ordering.API/Ordering.API.csproj set "Kestrel:Certificates:Default:Password" "developer" 
 dotnet user-secrets -p ApiGateways/YarpApiGateway/YarpApiGateway.csproj set "Kestrel:Certificates:Default:Path" "/home/app/.aspnet/https/localhost.pfx"
 dotnet user-secrets -p ApiGateways/YarpApiGateway/YarpApiGateway.csproj set "Kestrel:Certificates:Default:Password" "developer" 
-cp -fR ${HOME}/.microsoft/usersecrets/* /Microsoft/UserSecrets/ 
-chmod -R 777 /ASP.NET/
-chmod -R 777 /Microsoft/
+cp -fR ${HOME}/.microsoft/usersecrets/* ${APPDATA}/Microsoft/UserSecrets/ 
+chmod -R 777 ${APPDATA}/ASP.NET/
+chmod -R 777 ${APPDATA}/Microsoft/
